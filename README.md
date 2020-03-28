8d28ce89ccb80c603352fdd56423df3a4ea22041
adigreat <53363766+adigreat@users.noreply.github.com>
2020-03-28 17:58:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# solAR
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,34 @@
+var sun;
+var earth;
+
+function setup() {
+  createCanvas(500,500);
+
+}
+
+function draw() {
+  background(0,0,70);  
+  
+
+  fill('lightblue');
+
+  
+
+  var earth = circle(400,250,40);
+
+  fill('red');
+
+  circle(300,100,35);
+  
+  fill('yellow');
+  
+  var sun = circle(250,250,50 + frameCount);
+  
+  
+  
+  
+  
+
+
+ 
+}
